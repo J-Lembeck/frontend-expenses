8b71e071014f329e722a1ab6c4d5d97fa66693d4
matheusbruns <matheus.rbruns@gmail.com>
2023-05-10 23:46:23 -0300

    listagem do response da api em tela

--- src/pages/Expenses/Expenses.js
+++ src/pages/Expenses/Expenses.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../Components/Header";
 import api from "../../services/api";
 
 const Categories = () => {
 
+    const [expenses, setExpenses] = useState([]);
+    const [categories, setCategories] = useState([]);
+
     useEffect(() => {
-        // /find-all
 
         async function loadExpenses() {
-            const response = await api.get('/expenses/find-all');
-            const response2 = await api.get('/categories/find-all');
-            console.log(response);
-            console.log(response2);
+            const expensesResponse = await api.get('/expenses/find-all');
+            const categoriesResponse = await api.get('/categories/find-all');
+
+            setExpenses(expensesResponse.data);
+            setCategories(categoriesResponse.data)
         }
         loadExpenses();
     }, []);
@@ -19,7 +22,27 @@ const Categories = () => {
     return (
         <>
             <Header />
-            <h1>Despesas</h1>
+            <h1>{
+                expenses.map((expense) => {
+                    return (
+                        <div>
+                            <h1>{expense.description}</h1>
+                            <h2>{expense.value}</h2>
+                            <p>--------------------------------------------</p>
+                        </div>
+                    )
+                })
+            }</h1>
+            <h1>{
+                categories.map((categorie) => {
+                    return (
+                        <div>
+                            <h1>{categorie.description}</h1>
+                            <p>--------------------------------------------</p>
+                        </div>
+                    )
+                })
+            }</h1>
         </>
     )
 }
