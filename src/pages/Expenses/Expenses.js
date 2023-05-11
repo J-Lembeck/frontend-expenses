5dc2a9853ceab8c1d3badb5a118a2f335e56b9fa
matheusbruns <matheus.rbruns@gmail.com>
2023-05-10 23:24:08 -0300

    conexão com a api do back

--- src/pages/Expenses/Expenses.js
+++ src/pages/Expenses/Expenses.js
@@ -1,16 +1,24 @@
 import React, { useEffect } from "react";
 import Header from "../../Components/Header";
+import api from "../../services/api";
 
 const Categories = () => {
 
-    // useEffect(() => {
-    //     async function findAll
-    // });
+    useEffect(() => {
+        // /find-all
 
+        async function loadExpenses() {
+            const response = await api.get('/expenses/find-all');
+            const response2 = await api.get('/categories/find-all');
+            console.log(response);
+            console.log(response2);
+        }
+        loadExpenses();
+    }, []);
 
     return (
         <>
-            <Header/>
+            <Header />
             <h1>Despesas</h1>
         </>
     )
